c7a8c3b8f56984d8bb722945238cda0814043a9b
bnamgung@gmail.com <bnamgung@gmail.com>
2017-03-09 20:40:49 -0800

    pre launch

---
+++ app.js
@@ -0,0 +1,12 @@
+//map
+
+// var map;
+// function initMap(){
+//   map= new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 47.608013, lng: -122.335167}, //Seattle Location
+//     zoom: 8
+//   });
+// }
+//
+//
+// map= new google.maps.Map(document.getElementById("map"), {...});
