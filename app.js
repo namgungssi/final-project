13a470a0e1a10cacc01478d3aaa7743ec0b0ebce
bnamgung@gmail.com <bnamgung@gmail.com>
2017-03-18 10:14:26 -0700

    messed with

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ function initMap(){
     center: {lat: -33.9, lng: 151.2}
   });
 
-var DirectionsRequest(){
-  origin: LatLng | String | google.maps.Place,
-  destination: LatLng | String | google.maps.Place,
-  travelMode: TravelMode,
-  transitOptions: TransitOptions,
-  drivingOptions: DrivingOptions,
-  unitSystem: UnitSystem,
-  waypoints[]: DirectionsWaypoint,
-  optimizeWaypoints: Boolean,
-  provideRouteAlternatives: Boolean,
-  avoidHighways: Boolean,
-  avoidTolls: Boolean,
-  region: String
-}
-
-DirectionsService.route()
+//navigation with text attempt
+// var DirectionsRequest(){
+//   origin: LatLng | String | google.maps.Place,
+//   destination: LatLng | String | google.maps.Place,
+//   travelMode: TravelMode,
+//   transitOptions: TransitOptions,
+//   drivingOptions: DrivingOptions,
+//   unitSystem: UnitSystem,
+//   waypoints[]: DirectionsWaypoint,
+//   optimizeWaypoints: Boolean,
+//   provideRouteAlternatives: Boolean,
+//   avoidHighways: Boolean,
+//   avoidTolls: Boolean,
+//   region: String
+// }
+//
+// DirectionsService.route()
